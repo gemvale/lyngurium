90f0183b750797d398331045e377d0fa80af472b
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-03-05 02:31:19 +0800

    refactor: export `variables` from `ammolite`

--- packages/lynx/src/index.ts
+++ packages/lynx/src/index.ts
@@ -2,8 +2,7 @@ export type { VariablesObject, VariablesSetter } from "ammolite";
 
 export type { CSS, Keyframes } from "#/@types/style";
 
-export { merge, setVariables } from "ammolite";
+export { merge, setVariables, variables } from "ammolite";
 
 export { keyframes } from "#/functions/keyframes";
 export { style } from "#/functions/style";
-export { variables } from "#/functions/variables";
